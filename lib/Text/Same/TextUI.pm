aa602ecdf9d223401ad522c699b0ee4e75e6b050
kmr <kmr@f6704991-25e9-0310-81ae-dd6c47562deb>
2006-01-15 22:44:48 +0000

    Basic side-by-side view done.
    git-svn-id: file:///home/kmr44/Work/subversion_repository/psame/trunk@62 f6704991-25e9-0310-81ae-dd6c47562deb

--- lib/Text/Same/TextUI.pm
+++ lib/Text/Same/TextUI.pm
@@ -27,22 +27,85 @@ sub _draw_match_side_by_side
   my $options = shift;
   my $match = shift;
 
-  my $ret = "match " . $match->to_string . "\n";
+  my $min1 = $match->min1;
+  my $max1 = $match->max1;
+  my $min2 = $match->min2;
+  my $max2 = $match->max2;
 
-  my $start_context1 = _get_start_context($options, $match->min1, $match->source1);
-  my $start_context2 = _get_start_context($options, $match->min2, $match->source2);
+  my $width = $options->{term_width};
 
-  my $end_context1 = _get_end_context($options, $match->max1, $match->source1);
-  my $end_context2 = _get_end_context($options, $match->max2, $match->source2);
+  my $half_width = int($width / 2 - 6);
+
+  my $ret = "match " . ($min1+1) . ".." . ($max1+1) . "==" .
+            ($min2+1) . ".." . ($max1+1) . "\n";
+
+  my @start_context1 = _get_start_context($options, $min1, $match->source1);
+  my @start_context2 = _get_start_context($options, $min2, $match->source2);
+  my $max_start_context_len =
+    scalar(@start_context1) > scalar(@start_context2) ?
+      scalar(@start_context1) : scalar(@start_context2);
+
+  my $context_format_str =
+    "  %-${half_width}.${half_width}s   %-${half_width}.${half_width}s\n";
+  my $match_format_str =
+    "  %-${half_width}.${half_width}s = %-${half_width}.${half_width}s\n";
+  my $i;
+  for ($i = 0; $i < $max_start_context_len; $i++) {
+    if (defined $start_context1[$i] || defined $start_context2[$i]) {
+      $ret .= sprintf $context_format_str,
+        (defined $start_context1[$i] ? $start_context1[$i] : ""),
+        (defined $start_context2[$i] ? $start_context2[$i] : "");
+    }
+  }
+
+  my @match_chunks1 = _get_match_chunks($options, $min1, $max1,
+                                        $match->source1);
+  my @match_chunks2 = _get_match_chunks($options, $min2, $max2,
+                                        $match->source2);
+  my $max_match_len =
+    scalar(@match_chunks1) > scalar(@match_chunks2) ?
+      scalar(@match_chunks1) : scalar(@match_chunks2);
+
+  for ($i = 0; $i < $max_match_len; $i++) {
+    $ret .= sprintf $match_format_str,
+      (defined $match_chunks1[$i] ? $match_chunks1[$i] : ""),
+      (defined $match_chunks2[$i] ? $match_chunks2[$i] : "");
+  }
+
+  my @end_context1 = _get_end_context($options, $max1, $match->source1);
+  my @end_context2 = _get_end_context($options, $max2, $match->source2);
+  my $max_end_context_len =
+    scalar(@end_context1) > scalar(@end_context2) ?
+      scalar(@end_context1) : scalar(@end_context2);
+
+  for ($i = 0; $i < $max_end_context_len; $i++) {
+    if (defined $end_context1[$i] || defined $end_context2[$i]) {
+      $ret .= sprintf $context_format_str,
+        (defined $end_context1[$i] ? $end_context1[$i] : ""),
+        (defined $end_context2[$i] ? $end_context2[$i] : "");
+    }
+  }
 
 
+  return $ret;
+}
+
+sub _get_match_chunks
+{
+  my ($options, $min, $max, $source) = @_;
+
+  my @ret = ();
+
+  for (my $i = $min; $i <= $max; $i++) {
+    push @ret, ($source->get_all_chunks)[$i]->text;
+  }
+
+  return @ret;
 }
 
 sub _get_start_context
 {
-  my $options = shift;
-  my $min = shift;
-  my $source = shift;
+  my ($options, $min, $source) = @_;
 
   my $context_chunks = $options->{context} || 3;
   my $context_min = $min - $context_chunks;
@@ -62,16 +125,14 @@ sub _get_start_context
 
 sub _get_end_context
 {
-  my $options = shift;
-  my $max = shift;
-  my $source = shift;
+  my ($options, $max, $source) = @_;
 
   my $context_chunks = $options->{context} || 3;
   my $context_max = $max + $context_chunks;
 
   my @ret = ();
 
-  for (my $i = $max; $i < $context_max; $i++) {
+  for (my $i = $max + 1; $i < $context_max; $i++) {
     if ($i > $source->get_all_chunks_count - 1) {
       push @ret, undef;
     } else {
@@ -84,8 +145,7 @@ sub _get_end_context
 
 sub _draw_match_vertically
 {
-  my $options = shift;
-  my $match = shift;
+  my ($options, $match) = @_;
 
   my $ret = "match " . $match->to_string . "\n";
 
@@ -100,27 +160,24 @@ sub _draw_match_vertically
 
 sub _draw_range_and_context
 {
-  my $options = shift;
-  my $min = shift;
-  my $max = shift;
-  my $source = shift;
+  my ($options, $min, $max, $source) = @_;
 
   my $ret = "";
 
-  for my $chunk_text (_get_start_context($options, $min, $source)) {
-    if (defined $chunk_text) {
-      $ret .= "   $chunk_text\n";
+  for my $start_chunk_text (_get_start_context($options, $min, $source)) {
+    if (defined $start_chunk_text) {
+      $ret .= "   $start_chunk_text\n";
     }
   }
 
   for (my $i = $min; $i <= $max; $i++) {
-    my $chunk_text = ($source->get_all_chunks)[$i]->text;
-    $ret .= "=  $chunk_text\n";
+    my $match_chunk_text = ($source->get_all_chunks)[$i]->text;
+    $ret .= "=  $match_chunk_text\n";
   }
 
-  for my $chunk_text (_get_end_context($options, $max, $source)) {
-    if (defined $chunk_text) {
-      $ret .= "   $chunk_text\n";
+  for my $end_chunk_text (_get_end_context($options, $max, $source)) {
+    if (defined $end_chunk_text) {
+      $ret .= "   $end_chunk_text\n";
     }
   }
 

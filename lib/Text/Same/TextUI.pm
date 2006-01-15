1b46e47504009124a45cc64c9b337001079043d8
kmr <kmr@f6704991-25e9-0310-81ae-dd6c47562deb>
2006-01-15 21:24:15 +0000

    Rearranged ChunkedSource.pm and fixed some documentation.
    git-svn-id: file:///home/kmr44/Work/subversion_repository/psame/trunk@61 f6704991-25e9-0310-81ae-dd6c47562deb

--- lib/Text/Same/TextUI.pm
+++ lib/Text/Same/TextUI.pm
@@ -22,6 +22,66 @@ sub draw_match
   }
 }
 
+sub _draw_match_side_by_side
+{
+  my $options = shift;
+  my $match = shift;
+
+  my $ret = "match " . $match->to_string . "\n";
+
+  my $start_context1 = _get_start_context($options, $match->min1, $match->source1);
+  my $start_context2 = _get_start_context($options, $match->min2, $match->source2);
+
+  my $end_context1 = _get_end_context($options, $match->max1, $match->source1);
+  my $end_context2 = _get_end_context($options, $match->max2, $match->source2);
+
+
+}
+
+sub _get_start_context
+{
+  my $options = shift;
+  my $min = shift;
+  my $source = shift;
+
+  my $context_chunks = $options->{context} || 3;
+  my $context_min = $min - $context_chunks;
+
+  my @ret = ();
+
+  for (my $i = $context_min; $i < $min; $i++) {
+    if ($i < 0) {
+      push @ret, undef;
+    } else {
+      push @ret, ($source->get_all_chunks)[$i]->text;
+    }
+  }
+
+  return @ret;
+}
+
+sub _get_end_context
+{
+  my $options = shift;
+  my $max = shift;
+  my $source = shift;
+
+  my $context_chunks = $options->{context} || 3;
+  my $context_max = $max + $context_chunks;
+
+  my @ret = ();
+
+  for (my $i = $max; $i < $context_max; $i++) {
+    if ($i > $source->get_all_chunks_count - 1) {
+      push @ret, undef;
+    } else {
+      push @ret, ($source->get_all_chunks)[$i]->text;
+    }
+  }
+
+  return @ret;
+}
+
 sub _draw_match_vertically
 {
   my $options = shift;
@@ -45,28 +105,22 @@ sub _draw_range_and_context
   my $max = shift;
   my $source = shift;
 
-  my $context_lines = $options->{context_lines} || 3;
-
-  my $context_min = $min - $context_lines;
-  if ($context_min < 0) {
-    $context_min = 0;
-  }
-  my $context_max = $max + $context_lines;
-  if ($context_max > $source->get_all_chunks - 1) {
-    $context_max = scalar($source->get_all_chunks) - 1;
-  }
-
-  print "$min $max $context_min $context_max\n";
-
-
   my $ret = "";
 
-  for (my $i = $context_min; $i <= $context_max; $i++) {
-    my $chunk_text = ($source->get_all_chunks)[$i]->text;
-    if ($i < $min || $i > $max) {
+  for my $chunk_text (_get_start_context($options, $min, $source)) {
+    if (defined $chunk_text) {
       $ret .= "   $chunk_text\n";
-    } else {
-      $ret .= "=  $chunk_text\n";
+    }
+  }
+
+  for (my $i = $min; $i <= $max; $i++) {
+    my $chunk_text = ($source->get_all_chunks)[$i]->text;
+    $ret .= "=  $chunk_text\n";
+  }
+
+  for my $chunk_text (_get_end_context($options, $max, $source)) {
+    if (defined $chunk_text) {
+      $ret .= "   $chunk_text\n";
     }
   }
 
